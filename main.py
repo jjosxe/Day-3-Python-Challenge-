05e82361ca681a3d39d753655c19343a74f8b4e7
Rebeca Guijosa <113552170+Rguijosa@users.noreply.github.com>
2022-09-22 19:20:34 +0000

    we converted string to list

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 #jose, rebeca, katherine 
 
 
-Paragraph = "I pledge allegiance to the Flag of the United States of America, and to the Republic for which it stands, one Nation under God, indivisible, with liberty and justice for all."
+Paragraph = "I pledge allegiance to the Flag of the United States of America, and to the Republic for which it stands, one Nation under God, indivisible, with liberty and justice for all"
 
+ask1= input("Choose 1 letter from the Alphabet:   ")
+ask2= input("Choose 1 letter from the Alphabet:   ")
+ask3= input("Choose 1 letter from the Alphabet:   ")
 
+print(input("The letters you chose were: " + ask1 +ask2 + ask3))
 
-lst = []
-  
-# number of elements as input
-n = int(input("Choose a random letter: "))
-# Ask1= print(input("Choose 1 letter from the Alphabet:   "))
-# Ask2= print(input("Choose 1 letter from the Alphabet:   "))
-# Ask3= print(input("Choose 1 letter from the Alphabet:   "))
-
-letters = ["Ask1", "Ask2", "Ask3"]
-print(letters.count("Ask1"))
-print(letters.count("Ask2"))
-print(letters.count("Ask3"))
+letters= []
+letters.append(ask1)
+letters.append(ask2)
+letters.append(ask3)
+print(letters)
